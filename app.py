ed17b4247282e4cf9f02767e4b9af92f9ecfd1ce
Johna210 <solomonjohna21@gmal.com>
2023-09-18 15:53:55 +0300

    Started on the validation process of login and registration

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import os
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect,request, session
 from cs50 import SQL
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -18,5 +20,14 @@ Session(app)
 db = SQL("sqlite:///db/recipes.db")
 
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["Get","Post"])
+def login():
+    session.clear()
+
+    if request.method == "GET":
+        return render_template("login.html")
